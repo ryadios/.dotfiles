28636692a9397d142f248aac1738742e5707f29c
aditya <aditya.chaudhary0523@gmail.com>
2025-05-14 21:04:56 +0530

    feat(bin): add bin scripts & symlink

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/sh
+#!/bin/bash
 
 DOTFILES="$HOME/.dotfiles"
-CONFIG="$DOTFILES/.config"
 TARGET="$HOME"
-TARGET_CONFIG="$TARGET/.config"
+CONFIG="$TARGET/.config"
+LOCAL="$TARGET/.local"
 
 IGNORE=("README.md" "LICENSE" ".git" ".gitignore")
 
@@ -17,12 +17,22 @@ for file in "$DOTFILES"/.[^.]*; do
     ln -s "$file" "$TARGET/$name"
 done
 
-for file in "$CONFIG"/*; do
+for file in "$DOTFILES/.config"/*; do
     dir=$(basename "$file")
     # Skips files
     if [ -f "$file" ]; then
         continue
     fi
 
-    ln -s "$file" "$TARGET_CONFIG"
+    ln -s "$file" "$CONFIG"
+done
+
+for file in "$DOTFILES/bin"/*; do
+    name=$(basename "$file")
+    # Skips folder
+    if [ -d "$file" ]; then
+        continue
+    fi
+
+    ln -s "$file" "$LOCAL/bin/$name"
 done
