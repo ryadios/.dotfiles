3294df4fe54d90c3952c39e1ba70bfb56f8137e6
aditya <aditya.chaudhary0523@gmail.com>
2025-05-15 11:34:59 +0530

    chore(dots): use stow and clean old scripts

--- install.sh
+++ install.sh
@@ -1,38 +1,6 @@
 #!/bin/bash
 
-DOTFILES="$HOME/.dotfiles"
-TARGET="$HOME"
-CONFIG="$TARGET/.config"
-LOCAL="$TARGET/.local"
-
-IGNORE=("README.md" "LICENSE" ".git" ".gitignore")
-
-for file in "$DOTFILES"/.[^.]*; do
-    name=$(basename "$file")
-    # Skips ignored files and folders
-    if [[ " ${IGNORE[@]} " =~ " $name " ]] || [ -d "$file" ]; then
-        continue
-    fi
-
-    ln -s "$file" "$TARGET/$name"
-done
-
-for file in "$DOTFILES/.config"/*; do
-    dir=$(basename "$file")
-    # Skips files
-    if [ -f "$file" ]; then
-        continue
-    fi
-
-    ln -s "$file" "$CONFIG"
-done
-
-for file in "$DOTFILES/bin"/*; do
-    name=$(basename "$file")
-    # Skips folder
-    if [ -d "$file" ]; then
-        continue
-    fi
-
-    ln -s "$file" "$LOCAL/bin/$name"
-done
+[ -d "$HOME/.config" ] || mkdir -p "$HOME/.config"
+stow .config -t ~/.config
+stow .home -t ~
+stow bin -t ~/.local/bin
