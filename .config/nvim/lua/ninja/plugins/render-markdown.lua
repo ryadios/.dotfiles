cfc47f215ff9f775427dfee375dfe4fe7b8de1f6
aditya <aditya.chaudhary0523@gmail.com>
2025-05-15 11:24:02 +0530

    feat(nvim): add md preview plugin

--- .config/nvim/lua/ninja/plugins/render-markdown.lua
+++ .config/nvim/lua/ninja/plugins/render-markdown.lua
@@ -14,4 +14,13 @@ return {
         ---@type render.md.UserConfig
         opts = {},
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
 }
